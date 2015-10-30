64752530dcfa76ba8a9d1581d796248f2820d894
Jonathan Zong <jonathan@jonathanzong.com>
2015-10-30 15:14:00 -0400

    Initial Commit

---
+++ MaterialPalette/MaterialPalette.h
@@ -0,0 +1,19 @@
+//
+//  MaterialPalette.h
+//  MaterialPalette
+//
+//  Created by Jonathan Zong on 10/30/15.
+//  Copyright (c) 2015 Jonathan Zong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MaterialPalette.
+FOUNDATION_EXPORT double MaterialPaletteVersionNumber;
+
+//! Project version string for MaterialPalette.
+FOUNDATION_EXPORT const unsigned char MaterialPaletteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaterialPalette/PublicHeader.h>
+
+
